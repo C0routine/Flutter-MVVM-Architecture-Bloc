05c7b59f951c14861acda31cab127fd1b7e453cf
Casan <whdrjsco@gmail.com>
2024-08-10 20:20:48 +0900

    feature: CustomTheme Bloc 적용

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import 'package:architecture/core/config/style/app_style.dart';
+import 'package:architecture/core/config/app_style.dart';
+import 'package:architecture/core/config/app_theme.dart';
+import 'package:architecture/core/router/router_config.dart';
+import 'package:architecture/feature/theme/bloc/barrel.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
 
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
@@ -15,7 +24,18 @@ class App extends StatelessWidget {
       ),
       minTextAdapt: true,
       builder: (BuildContext context, Widget? child) {
-        return MaterialApp();
+        return BlocProvider(
+          lazy: false,
+          create: (BuildContext context) => ThemeBloc(),
+          child: BlocBuilder<ThemeBloc, ThemeState>(
+            builder: (BuildContext context, state) {
+              return MaterialApp.router(
+                theme: customAppTheme(state.type),
+                routerConfig: routerConfig,
+              );
+            },
+          ),
+        );
       },
     );
   }
