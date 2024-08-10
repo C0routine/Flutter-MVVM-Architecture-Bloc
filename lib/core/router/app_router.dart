d622af163413af490d37a35482a72757bdff1103
Casan <whdrjsco@gmail.com>
2024-08-10 12:50:46 +0900

    feature: Router 관리 AppRouter 정의

---
+++ lib/core/router/app_router.dart
@@ -0,0 +1,26 @@
+enum AppRouter {
+  root(path: '/', name: 'root', routerConfigPath: '/'),
+
+  /// Home Router
+  home(path: '/home', name: 'home', routerConfigPath: '/home'),
+
+  /// Tab Router
+  tab2(path: '/tab2', name: 'tab2', routerConfigPath: '/tab2'),
+  tab3(path: '/tab3', name: 'tab3', routerConfigPath: '/tab3'),
+  tab4(path: '/tab4', name: 'tab4', routerConfigPath: '/tab4'),
+
+  /// My Router
+  my(path: '/my', name: 'my', routerConfigPath: '/my'),
+  auth(path: '/my/auth', name: 'auth', routerConfigPath: '/my/auth'),
+  example(path: '/example', name: 'example', routerConfigPath: '/example');
+
+  const AppRouter({
+    required this.path,
+    required this.name,
+    required this.routerConfigPath,
+  });
+
+  final String path;
+  final String name;
+  final String routerConfigPath;
+}
