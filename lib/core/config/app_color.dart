47de214c5ef5b53caf352ea77d0cbded98b8f9e3
Casan <whdrjsco@gmail.com>
2024-08-18 15:44:52 +0900

    fix: Light, Dark Color 정의변경

--- lib/core/config/app_color.dart
+++ lib/core/config/app_color.dart
@@ -1,5 +1,5 @@
-import 'package:architecture/feature/theme/bloc/barrel.dart';
 import 'package:flutter/material.dart';
+import 'package:architecture/feature/theme/bloc/barrel.dart';
 
 /// AppColor 추상 클래스 (Dark, Light 모두 사용 가능한 AppColor 를 구현)
 /// [theme] 값에 따라 DarkAppColor 또는 LightAppColor instance 를 반환
@@ -12,8 +12,19 @@ abstract class AppColor {
     }
   }
 
-  abstract final Color background;
-  abstract final Color textPrimary;
+  abstract final Color bg100;
+  abstract final Color bg200;
+  abstract final Color bg300;
+
+  abstract final Color primary100;
+  abstract final Color primary200;
+  abstract final Color primary300;
+
+  abstract final Color accent100;
+  abstract final Color accent200;
+
+  abstract final Color textMain;
+  abstract final Color textSub;
 }
 
 class LightAppColor implements AppColor {
@@ -26,10 +37,34 @@ class LightAppColor implements AppColor {
   }
 
   @override
-  Color get background => Colors.white;
+  Color get accent100 => const Color(0xFF71c4ef); // #71c4ef
 
   @override
-  Color get textPrimary => Colors.black;
+  Color get accent200 => const Color(0xFF00668c); // #00668c
+
+  @override
+  Color get bg100 => const Color(0xFFfffefb); // #fffefb
+
+  @override
+  Color get bg200 => const Color(0xFFf5f4f1); // #f5f4f1
+
+  @override
+  Color get bg300 => const Color(0xFFcccbc8); // #cccbc8
+
+  @override
+  Color get primary100 => const Color(0xFFd4eaf7); // #d4eaf7
+
+  @override
+  Color get primary200 => const Color(0xFFb6ccd8); // #b6ccd8
+
+  @override
+  Color get primary300 => const Color(0xFF3b3c3d); // #3b3c3d
+
+  @override
+  Color get textMain => const Color(0xFF1d1c1c); // #1d1c1c
+
+  @override
+  Color get textSub => const Color(0xFF313d44); // #313d44
 }
 
 class DarkAppColor implements AppColor {
@@ -42,8 +77,32 @@ class DarkAppColor implements AppColor {
   }
 
   @override
-  Color get background => Colors.black;
+  Color get accent100 => const Color(0xFF006fff); // #006fff
 
   @override
-  Color get textPrimary => Colors.white;
+  Color get accent200 => const Color(0xFFe1ffff); // #e1ffff
+
+  @override
+  Color get bg100 => const Color(0xFF1E1E1E); // #1E1E1E
+
+  @override
+  Color get bg200 => const Color(0xFF2d2d2d); // #2d2d2d
+
+  @override
+  Color get bg300 => const Color(0xFF454545); // #454545
+
+  @override
+  Color get primary100 => const Color(0xFF0085ff); // #0085ff
+
+  @override
+  Color get primary200 => const Color(0xFF69b4ff); // #69b4ff
+
+  @override
+  Color get primary300 => const Color(0xFFe0ffff); // #e0ffff
+
+  @override
+  Color get textMain => const Color(0xFFffffff); // #ffffff
+
+  @override
+  Color get textSub => const Color(0xFF9e9e9e); // #9e9e9e
 }
--- lib/core/config/app_theme.dart
+++ lib/core/config/app_theme.dart
@@ -1,23 +1,25 @@
+import 'package:flutter/material.dart';
+
 import 'package:architecture/core/config/app_color.dart';
 import 'package:architecture/feature/theme/bloc/barrel.dart';
-import 'package:flutter/material.dart';
 
 ThemeData customAppTheme(ThemeType type) {
   /// 현재 테마에 맞는 AppColor instance 생성
   final appColor = AppColor(type: type);
 
   return ThemeData(
-    scaffoldBackgroundColor: appColor.background,
+    scaffoldBackgroundColor: appColor.bg100,
     extensions: <ThemeExtension>[
-      CustomAppThemeExtension(appColor: appColor),
+      CustomAppThemeExtension(appColor: appColor, themeType: type),
     ],
   );
 }
 
 class CustomAppThemeExtension extends ThemeExtension<CustomAppThemeExtension> implements AppColor {
-  CustomAppThemeExtension({required this.appColor});
+  CustomAppThemeExtension({required this.appColor, required this.themeType});
 
   final AppColor appColor;
+  final ThemeType themeType;
 
   @override
   ThemeExtension<CustomAppThemeExtension> copyWith() {
@@ -29,11 +31,39 @@ class CustomAppThemeExtension extends ThemeExtension<CustomAppThemeExtension> im
     return this;
   }
 
-  @override
-  Color get background => appColor.background;
+  bool get isDark => themeType == ThemeType.dark;
+
+  bool get isLight => themeType == ThemeType.light;
 
   @override
-  Color get textPrimary => appColor.textPrimary;
+  Color get accent100 => appColor.accent100;
+
+  @override
+  Color get accent200 => appColor.accent200;
+
+  @override
+  Color get bg100 => appColor.bg100;
+
+  @override
+  Color get bg200 => appColor.bg200;
+
+  @override
+  Color get bg300 => appColor.bg300;
+
+  @override
+  Color get primary100 => appColor.primary100;
+
+  @override
+  Color get primary200 => appColor.primary200;
+
+  @override
+  Color get primary300 => appColor.primary300;
+
+  @override
+  Color get textMain => appColor.textMain;
+
+  @override
+  Color get textSub => appColor.textSub;
 }
 
 extension ThemeShortcut on BuildContext {
