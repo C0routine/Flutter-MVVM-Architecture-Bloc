0a17d3b9d97cf36ec200589bf488d4bc3854ea99
Casan <whdrjsco@gmail.com>
2024-10-09 16:22:27 +0900

    feature: skeletonizer 적용

--- lib/feature/home/screen/home_screen.dart
+++ lib/feature/home/screen/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+
+import 'package:architecture/core/configs/app_theme.dart';
 import 'package:architecture/core/common/default_screen.dart';
 import 'package:architecture/feature/home/widgets/theme_switch.dart';
 
@@ -17,6 +20,13 @@ class _HomeScreenState extends State<HomeScreen> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           const ThemeSwitchWidget(),
+          Skeletonizer(
+            child: Container(
+              width: 300,
+              height: 200,
+              color: context.appColor.primary200,
+            ),
+          ),
         ],
       ),
     );
