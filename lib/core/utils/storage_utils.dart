9c7fcc25c7c2143c02de23c4215dd50ef2bd4144
Casan <whdrjsco@gmail.com>
2024-10-13 18:38:20 +0900

    feature: SecureStorage 적용

---
+++ lib/core/utils/storage_utils.dart
@@ -0,0 +1,4 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+const FlutterSecureStorage storage = FlutterSecureStorage();
+const storageIOSOptions = IOSOptions(accessibility: IOSAccessibility.first_unlock);
